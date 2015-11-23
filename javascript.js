6aa98bf019462e88a00f757f7f0a6a7d1083070d
Borja Díaz de Terán <borja@promosay.com>
2015-11-22 20:31:43 -0500

    Adds example of conversion tracking code firing onclick within different tab

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//fire Floodlight on click
+$(document).on('click', '#link', function(){
+	//fire Floodlight
+	floodlight();
+});
+
+//append floodlight iframe to body
+function floodlight(){
+	var axel = Math.random() + "";
+	var a = axel * 10000000000000;
+	$('body').append('<iframe src="https://12345.fls.doubleclick.net/activityi;src=12345;type=nhtsa00;cat=nhtsa0;ord=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+}
